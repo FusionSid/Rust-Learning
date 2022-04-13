31eb66492867cc5660f920d81f5d1d3cd1cd175e
FusionSid <siddheshadsv@icloud.com>
2022-04-13 16:35:09 +1200

    Fixed comment

--- src/date_types.rs
+++ src/date_types.rs
@@ -1,29 +1,32 @@
 /*
 Integer - i<size> or u<size>
-
 (i means signed | u for unsigned)
+
+Float - f<size>
 */
 
 pub fn main() {
     let hello = "Hello from data_types.rs!";
     println!("\n{}\n{}", hello, "-".repeat(hello.len()));
 
-    // Unsigned
     let age: i16;
+    let name: &str;
     let rating: f32;
     let letter: char;
     let sid_is_cool: bool;
-
-    // Signed
+    
     age = 10; // integer
+    name = "Siddhesh"; // string
     rating = 4.5; // float
     letter = 'a'; // char (single quote)
     sid_is_cool = true; // bool
     
-    println!("Age: {}\nRating: {}\nLetter: {}\nSid_is_cool: {}", age, rating, letter, sid_is_cool);
+    println!("Age: {}\nName: {}\nRating: {}\nLetter: {}\nSid_is_cool: {}", age, name, rating, letter, sid_is_cool);
     
     // If you want to make it mutable use the mut keyword
-    let mut name = String::new(); // i couldnt find how to do this so i think this is how
-    name.push_str("Siddhesh"); // string (double quote)
-    println!("Name: {}", name)
+    let mut name_2 = "Siddhesh";
+    println!("Name: {}", name_2);
+
+    name_2 = "FusionSid";
+    println!("Name: {}", name_2);
 }
